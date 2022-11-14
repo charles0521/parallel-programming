145c99001252ce6de49503e385c24e51cc573bb9
charles0521 <ma880521@gmail.com>
2022-11-14 16:03:06 +0800

    add pagerank

--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -28,6 +28,63 @@ void pageRank(Graph g, double *solution, double damping, double convergence)
     solution[i] = equal_prob;
   }
 
+  double *score_new = (double *)malloc(sizeof(double) * numNodes);
+  double *score_old = (double *)malloc(sizeof(double) * numNodes);
+  double sum_no_outgoing;
+
+  //init
+  for(int i=0; i<numNodes; ++i)
+  {
+    score_old[i] = equal_prob;
+  }
+
+
+  // #pragma omp for
+  double global_diff;
+
+  do
+  {
+    #pragma omp parallel for
+    for(int i=0; i<numNodes; ++i)
+    {
+      score_new[i] = 0.0;
+      for(int j=0; j<incoming_size(g, i); ++j)
+      {
+        int intcoming_point = g->incoming_edges[g->incoming_starts[i] + j];
+        score_new[i] += score_old[intcoming_point]/outgoing_size(g, intcoming_point);
+      }
+      score_new[i] = (damping * score_new[i]) + (1.0 - damping) / numNodes;
+    }
+
+    sum_no_outgoing = 0.0;
+    // #pragma omp parallel for
+    for(int i=0; i<numNodes; ++i)
+    {
+      if(outgoing_size(g, i) == 0)
+        sum_no_outgoing = sum_no_outgoing + (damping * score_old[i] / numNodes);
+    }
+    #pragma omp parallel for
+    for(int i=0; i<numNodes; ++i)
+    {
+      score_new[i] += sum_no_outgoing;
+    }
+
+    global_diff = 0.0;
+    for(int i=0; i<numNodes; ++i)
+    {
+      global_diff += abs(score_new[i] - score_old[i]);
+      score_old[i] = score_new[i];
+    }
+
+  }while(!(global_diff < convergence));
+  #pragma omp parallel for
+  for(int i=0; i<numNodes; ++i)
+  {
+    solution[i] = score_new[i];
+  }
+
+  free(score_new);
+  free(score_old);
   /*
      For PP students: Implement the page rank algorithm here.  You
      are expected to parallelize the algorithm using openMP.  Your
